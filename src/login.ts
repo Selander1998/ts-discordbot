04f9deaf5ec0c794a91cabb4af773ab86ff42a9d
Selander1998 <selanderantonsson@gmail.com>
2023-02-10 23:27:01 +0100

    file setup

---
+++ src/login.ts
@@ -0,0 +1,18 @@
+import { ActivityType } from "discord.js";
+import { client } from "./main";
+
+client.once("ready", (botClient) => {
+  if (botClient) {
+    botClient.user.setPresence({
+      activities: [{ name: "Alla busiga elever", type: ActivityType.Watching }],
+      status: "online",
+    });
+    console.log(
+      `Bot successfully loaded its client and is now logged in as: ${botClient.user.tag}`
+    );
+  } else {
+    console.error("Bot did not correctly load its client");
+  }
+});
+
+client.login("NzYzNzQxMTM5MjQ0OTQxMzMy.X38HXQ.kXrG7VDNP3zIEfYYSqXfI_XZVSA");
