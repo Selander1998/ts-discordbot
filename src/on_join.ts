e9cc9d97cce514310b0bb4411eccf218ae5b2fe1
Selander1998 <selanderantonsson@gmail.com>
2023-12-06 19:36:46 +0100

    Role ID instead of string check

--- src/on_join.ts
+++ src/on_join.ts
@@ -3,20 +3,20 @@ export class UserJoined {
 	private constructor(client: Client) {
 		client.on("guildMemberAdd", (member: GuildMember) => {
 			const guild = client.guilds.cache.get(member.guild.id);
-			const roleName = "1-åring";
+			const roleId = "514826778057900041";
 
 			if (guild) {
-				const roleToGive = guild.roles.cache.find((role) => role.name === roleName);
+				const roleToGive = guild.roles.cache.find((role) => role.id === roleId);
 
 				if (roleToGive) {
 					if (member.roles.cache.has(roleToGive.id)) {
 						return console.log(
-							`${member.displayName} already has the role ${roleName}, aborting addition`
+							`${member.displayName} already has the role ${roleToGive.name}, aborting addition`
 						);
 					}
 					member.roles.add(roleToGive.id);
 				} else {
-					console.log(`Could not add role ${roleName} to newly joined user: ${member.displayName}`);
+					console.log(`Could not add role ${roleId} to newly joined user: ${member.displayName}`);
 				}
 			} else {
 				console.log(`Guild missing, got value of: ${guild}`);
