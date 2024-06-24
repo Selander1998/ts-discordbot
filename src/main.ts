becb465a845198ea2efa647f5a31934ff80c2d18
Selander1998 <selanderantonsson@gmail.com>
2024-06-23 20:13:03 -0400

    Some sort of sequelize wrapper implemented

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { Client, GatewayIntentBits, Partials, ActivityType, Events } from "disco
 import { UserJoined } from "./on_join";
 import { MessageListener } from "./message";
 import { ReactionListener } from "./reaction";
+import { SequelizeWrapper } from "./sequelize";
 
-const logging = true;
+export const logging = true;
 
 export const client = new Client({
 	intents: [
 		GatewayIntentBits.DirectMessages,
 		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildBans,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
 		GatewayIntentBits.GuildMessageReactions,
@@ -22,27 +22,27 @@ export const client = new Client({
 
 client.once("ready", (botClient: Client<true>) => {
 	if (!botClient) {
-		console.error("Bot did not correctly load its client");
+		console.error("Bot did not correctly load its client, exiting process");
 		process.exit(0);
 	}
+	console.log(`Bot successfully loaded its client and is now logged in as: ${botClient.user.tag}`);
 
 	botClient.user.setPresence({
-		activities: [{ name: "alla busiga elever", type: ActivityType.Watching }],
+		activities: [{ name: "all the naughties", type: ActivityType.Watching }],
 		status: "online",
 	});
-	console.log(`Bot successfully loaded its client and is now logged in as: ${botClient.user.tag}`);
-	setImmediate(() => {
-		if (logging) {
-			client.on(Events.MessageReactionAdd, async (reaction, user) => {
-				console.log(`LOG: ${user.username} reacted with: ${reaction.emoji}`);
-			});
-		}
-	});
 
-	UserJoined.init(client);
-	MessageListener.init(client);
-	ReactionListener.init(client);
-	console.log("Ran init funcs");
+	SequelizeWrapper.init() || console.error("LOG: SequelizeWrapper failed to initialize properly");
+
+	UserJoined.init(client) || console.error("LOG: UserJoined failed to initialize properly");
+
+	MessageListener.init(client) ||
+		console.error("LOG: MessageListener failed to initialize properly");
+
+	ReactionListener.init(client) ||
+		console.error("LOG: ReactionListener failed to initialize properly");
+
+	if (logging) console.log("LOG: All init functions ran successfully");
 });
 
 client.login("NzYzNzQxMTM5MjQ0OTQxMzMy.X38HXQ.kXrG7VDNP3zIEfYYSqXfI_XZVSA");
---
+++ src/sequelize.ts
@@ -0,0 +1,80 @@
+import { Sequelize, DataTypes } from "sequelize";
+import dotenv from "dotenv";
+
+const envConfig = dotenv.config();
+const queryTypes = require("sequelize"); // Does not work with import("sequelize"), don´t know why. Common JS is just to good i guess?
+
+export class SequelizeWrapper {
+	private static instance: Sequelize;
+
+	private constructor() {
+		console.log("Noticed SequelizeWrapper constructor");
+
+		console.log(envConfig.error);
+		console.log(envConfig.parsed);
+
+		SequelizeWrapper.instance = new Sequelize(
+			process.env.DB_NAME as string,
+			process.env.DB_USER as string,
+			process.env.DB_PASSWORD as string,
+			{
+				host: process.env.DB_NAME as string,
+				dialect: process.env.DB_NAME as any,
+			}
+		);
+
+		SequelizeWrapper.instance
+			.authenticate()
+			.then(() => {
+				console.log("Database connection has been established successfully.");
+			})
+			.catch((error) => {
+				console.error("Unable to connect to the database:", error);
+			});
+
+		if (process.env.DB_CREATE) {
+			console.log("Setting up database tables...");
+			SequelizeWrapper.generateTables();
+		}
+	}
+
+	public static generateTables() {
+		this.instance.define(
+			"roles",
+			{
+				roleId: {
+					type: DataTypes.STRING,
+					allowNull: false,
+					primaryKey: true,
+				},
+				emojiId: {
+					type: DataTypes.STRING,
+					allowNull: false,
+				},
+			},
+			{}
+		);
+
+		this.instance.sync().then(() => {
+			console.log("Database tables have been generated");
+		});
+	}
+
+	public static async fetchRoles() {
+		const rolesData = await this.instance.query("SELECT * FROM roles", {
+			type: queryTypes.SELECT,
+		});
+		return rolesData;
+	}
+
+	public static async addRole(roleId: string, emojiId: string) {
+		this.instance.query("INSERT INTO `roles` (roleId, emojiId) VALUES (?, ?)", {
+			replacements: [roleId, emojiId],
+			type: queryTypes.INSERT,
+		});
+	}
+
+	public static init() {
+		return new SequelizeWrapper();
+	}
+}
