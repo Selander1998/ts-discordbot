189c8d2c2423d059790da6dea7cf78c739f06d24
Selander1998 <selanderantonsson@gmail.com>
2024-07-04 05:40:20 -0400

    Added workaround for interaction limit of 5 on messages

--- src/rolemanager.ts
+++ src/rolemanager.ts
@@ -1,4 +1,11 @@
-import { Client, ButtonBuilder, ButtonStyle, ActionRowBuilder, Message } from "discord.js";
+import {
+	Client,
+	ButtonBuilder,
+	ButtonStyle,
+	ActionRowBuilder,
+	Message,
+	MessageActionRowComponentBuilder,
+} from "discord.js";
 import { Role } from "./dbmodels/role";
 
 export class RoleManager {
@@ -26,9 +33,11 @@ export class RoleManager {
 		if (!channel) return "Juste, channel kan också vara null säger Jabok";
 
 		if (channel.isTextBased()) {
-			const rolesToCreate: ButtonBuilder[] = [];
+			let rolesToCreate: ButtonBuilder[] = [];
+			const actionRows: ActionRowBuilder<MessageActionRowComponentBuilder>[] = [];
 			const roles = await this.getRolesFromDb();
 
+			let count = 0;
 			for (const role of roles) {
 				rolesToCreate.push(
 					new ButtonBuilder()
@@ -36,15 +45,26 @@ export class RoleManager {
 						.setLabel(role.buttonLabel)
 						.setStyle(ButtonStyle.Primary)
 				);
+				count++;
+				if (count >= 5) {
+					actionRows.push(new ActionRowBuilder<ButtonBuilder>().addComponents(...rolesToCreate));
+					count = 0;
+					rolesToCreate.length = 0;
+				}
 			}
 
-			if (rolesToCreate.length <= 0)
+			// If there´s still roles left behind
+			if (rolesToCreate.length > 0) {
+				actionRows.push(new ActionRowBuilder<ButtonBuilder>().addComponents(...rolesToCreate));
+			}
+
+			if (rolesToCreate.length <= 0 && actionRows.length <= 0)
 				return console.error("No roles found in database, aborting sending interactive message");
 
 			this.activeMessage = await channel.send({
 				content:
 					"Klicka på knapparna nedan för att få respektive roll och med det tillgång till dess kanaler.",
-				components: [new ActionRowBuilder<ButtonBuilder>().addComponents(...rolesToCreate)],
+				components: [...actionRows],
 			});
 		}
 	}
